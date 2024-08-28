15dbaa6b04316fa366213c460134de36d475d7a2
skyline8880 <skyline89_88@mail.ru>
2024-08-28 18:06:38 +0300

    init

--- main.py
+++ main.py
@@ -33,4 +33,4 @@ async def get_deal_data(deal_id: int, department_id: int):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app=app, host='127.0.0.1', port=8887)
+    uvicorn.run(app=app, host='0.0.0.0', port=8887)
